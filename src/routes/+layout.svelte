e6e2b3694115245e376fbc0bab703c83d997d6ba
MichailoOvchar <misha.ovchar23@gmail.com>
2023-05-06 16:56:44 +0300

    Start new project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import "../style/app.scss";
+</script>
+
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="/logo.png" alt="X" />
+            <div class="info">
+                <h1>Svelte pizza</h1>
+                <div class="description">Сама смачна піца</div>
+            </div>
+        </div>
+        <div class="button">
+            <button class="btn orange">Button</button>
+        </div>
+    </div>
+</header>
+
+<slot />
+
+<footer>
+    <div class="container">Made by ItSchool</div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import PizzaBlock from "./PizzaBlock.svelte";
+</script>
+
+<section id="menu">
+    <div class="container">
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+        <PizzaBlock />
+    </div>
+</section>
